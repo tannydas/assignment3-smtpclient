0300c46f0dd514462eea6cc16648ac32b3fd32a0
tannydas <79417918+tannydas@users.noreply.github.com>
2022-02-23 23:23:11 -0500

    Update to QUIT made

--- solution.py
+++ solution.py
@@ -62,6 +62,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     quitCommand = 'QUIT\r\n'
     clientSocket.send(quitCommand.encode())
+    recv5 = clientSocket.recv(1024).decode()
     # Fill in end
 
 
